c676a47ed65e3d34783d5b0b5adf9c615fddc961
Alistair Abao <abaolester@gmail.com>
2024-05-22 15:36:04 +0200

    env changes

--- app.js
+++ app.js
@@ -1,98 +1,94 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import path from 'path';
-import ejsMate from 'ejs-mate';
-import { fileURLToPath } from 'url';
-import flash from 'connect-flash'
-import session from 'cookie-session'
-import multer from 'multer'
-import methodOverride from 'method-override'
-import main from './routes/main.js';
-import user from './routes/user.js';
-import ExpressError from './utils/expressError.js'
-import dotenv from 'dotenv'
-dotenv.config()
+import path from "path";
+import ejsMate from "ejs-mate";
+import { fileURLToPath } from "url";
+import flash from "connect-flash";
+import session from "cookie-session";
+import multer from "multer";
+import methodOverride from "method-override";
+import main from "./routes/main.js";
+import user from "./routes/user.js";
+import ExpressError from "./utils/expressError.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 // import { getHours, getUser, getAll } from './database.js'
 
-
-
 // configure the storage object where to store the images
-// cb is a callback function 
+// cb is a callback function
 const storage = multer.diskStorage({
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-    }
-})
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
 
-
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-app.engine('ejs', ejsMate)
-
-app.use(flash())
-app.use(express.urlencoded({ extended: true }))//parse form data
-app.use(express.static('public'))
-app.use(methodOverride('_method'))
+app.use(flash());
+app.use(express.urlencoded({ extended: true })); //parse form data
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 // starting session config
 app.use(
-    session({
-        secret: 'secret',
-        saveUninitialized: false,
-        resave: false,
-        cookie: {
-            //setup expiry for the session cookie
-            //1000ms to 1s to 1min to 24hrs to 1wk
+  session({
+    secret: "secret",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      //setup expiry for the session cookie
+      //1000ms to 1s to 1min to 24hrs to 1wk
 
-            expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-            maxAge: 1000 * 60 * 60 * 24 * 7,
-            secure: true,
-            httpOnly: true // sets cookie so that it is impossible to access clientside
-        }
-    })
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      secure: true,
+      httpOnly: true, // sets cookie so that it is impossible to access clientside
+    },
+  })
 );
 
 /* MIDDLEWARE */
 //storing the req.flash to locals to gain access anywhere
 app.use((req, res, next) => {
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error')
-    //checking for logged user
-    res.locals.isLoggedUser = req.session.user_id
-    next()
-})
-
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  //checking for logged user
+  res.locals.isLoggedUser = req.session.user_id;
+  next();
+});
 
 // using routes
-app.use('/main', main);
-app.use('/user', user);
-
+app.use("/main", main);
+app.use("/user", user);
 
 // ROUTES
 //restructured routes into main and users
 
 // error handler
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page not Found', 404))//passing the error to the next error handler
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page not Found", 404)); //passing the error to the next error handler
+});
 
 app.use((err, req, res, next) => {
-    const { status = 404 } = err
-    if (!err.message) {
-        err.message = "Oooops there's an error"
-    } else {
-        res.status(status).render('errorAlert.ejs', { err })
-    }
-})
+  const { status = 404 } = err;
+  if (!err.message) {
+    err.message = "Oooops there's an error";
+  } else {
+    res.status(status).render("errorAlert.ejs", { err });
+  }
+});
 //check
-const port = process.env.DB_PORT
+const port = process.env.MYSQL_PORT;
 app.listen(port, () => {
-    console.log(`SERVING PORT ${port}`)
-})
+  console.log(`SERVING PORT ${port}`);
+});
--- database.js
+++ database.js
@@ -1,86 +1,95 @@
-import mysql from 'mysql2'
-import dotenv from 'dotenv'
-dotenv.config()
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
 
-
-const pool = mysql.createPool({
+const pool = mysql
+  .createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port: process.env.DB_PORT
-}).promise()
-
+    // port: process.env.MYSQL_PORT,
+  })
+  .promise();
 
 export async function getImages(id) {
-    const [image] = await pool.query(`
+  const [image] = await pool.query(
+    `
         SELECT * FROM images
-        WHERE id = ?`, [id]
-    )
-    return image[0]
+        WHERE id = ?`,
+    [id]
+  );
+  return image[0];
 }
 // // const result = await getImages(3)
 // // console.log(result)
 
 // selecting all images
 export async function getAll() {
-    const [allImages] = await pool.query(`
-        SELECT * FROM images`
-    )
-    return allImages[0]
+  const [allImages] = await pool.query(`
+        SELECT * FROM images`);
+  return allImages[0];
 }
 
 // selecting all menu
 export async function getMenu() {
-    const allMenu = await pool.query(
-        `SELECT title, description, price, category
+  const allMenu = await pool.query(
+    `SELECT title, description, price, category
         FROM menu`
-    )
-    return allMenu[0]
+  );
+  return allMenu[0];
 }
 // const result = await getMenu()
 // console.log(result)
 
 // selecting entrees from menu
 export async function getEntrees() {
-    const allEntrees = await pool.query(
-        `SELECT * FROM menu
+  const allEntrees = await pool.query(
+    `SELECT * FROM menu
         WHERE category = 'Entrées'`
-    )
-    return allEntrees[0]
+  );
+  return allEntrees[0];
 }
-
 
 // selecting plats from the menu
 export async function getPlats() {
-    const allPlats = await pool.query(
-        `SELECT * FROM menu
+  const allPlats = await pool.query(
+    `SELECT * FROM menu
         WHERE category = 'Plats'`
-    )
-    return allPlats[0]
+  );
+  return allPlats[0];
 }
 
 // selecting desserts from menu
 export async function getDesserts() {
-    const allDesserts = await pool.query(
-        `SELECT * FROM menu
+  const allDesserts = await pool.query(
+    `SELECT * FROM menu
         WHERE category = 'Desserts'`
-    )
-    return allDesserts[0]
+  );
+  return allDesserts[0];
 }
 
 // inserting data from reservation form
 //logic for inserting a new reservation using the date,miditime, soirtime,
 //covers, name, email as the parameters, then using them in an array
-//for the insert query(this means that these are the datas that we 
+//for the insert query(this means that these are the datas that we
 //will be accepting from the forms)
 //Update: included user_id
-export async function createReservation(date, time, covers, name, email, user_id, allergies) {
-    const newReservation = await pool.query(
-        `INSERT INTO reservations(date, time, covers, name, email, user_id, allergies)
-        VALUES(?, ?, ?, ?, ?, ?, ?)`, [date, time, covers, name, email, user_id, allergies]
-    )
-    return newReservation[0]
+export async function createReservation(
+  date,
+  time,
+  covers,
+  name,
+  email,
+  user_id,
+  allergies
+) {
+  const newReservation = await pool.query(
+    `INSERT INTO reservations(date, time, covers, name, email, user_id, allergies)
+        VALUES(?, ?, ?, ?, ?, ?, ?)`,
+    [date, time, covers, name, email, user_id, allergies]
+  );
+  return newReservation[0];
 }
 
 // inserting data from the register form
@@ -88,20 +97,28 @@ export async function createReservation(date, time, covers, name, email, user_id
 //These arguments will come from the register form
 //added 2 new columns in the database( covers and allergies)
 export async function register(email, password, covers, allergies) {
-    const newRegister = await pool.query(
-        `INSERT INTO users(email, password, covers, allergies)
-        VALUES(?, ?, ?, ?)`, [email, password, covers, allergies]
-    )
-    return newRegister[0]
+  const newRegister = await pool.query(
+    `INSERT INTO users(email, password, covers, allergies)
+        VALUES(?, ?, ?, ?)`,
+    [email, password, covers, allergies]
+  );
+  return newRegister[0];
 }
 
 //adding admin user
-export async function registerAdmin(email, password, covers, account_type, allergies) {
-    const newRegisterAdmin = await pool.query(
-        `INSERT INTO users(email, password, covers, account_type,allergies)
-        VALUES(?, ?, ?, ?, ?)`, [email, password, covers, account_type, allergies]
-    )
-    return newRegisterAdmin[0]
+export async function registerAdmin(
+  email,
+  password,
+  covers,
+  account_type,
+  allergies
+) {
+  const newRegisterAdmin = await pool.query(
+    `INSERT INTO users(email, password, covers, account_type,allergies)
+        VALUES(?, ?, ?, ?, ?)`,
+    [email, password, covers, account_type, allergies]
+  );
+  return newRegisterAdmin[0];
 }
 
 // adding default_covers and allergies
@@ -113,107 +130,123 @@ export async function registerAdmin(email, password, covers, account_type, aller
 //     return defaults[0]
 // }
 
-// searching for a username 
+// searching for a username
 export async function login(email, password) {
-    const foundLogin = await pool.query(
-        `SELECT * FROM users
-        WHERE email = ?`, [email]
-    )
-    return foundLogin[0]
-};
+  const foundLogin = await pool.query(
+    `SELECT * FROM users
+        WHERE email = ?`,
+    [email]
+  );
+  return foundLogin[0];
+}
 
 // search a specific photo
 export async function getPhoto(id) {
-    const newPhoto = await pool.query(
-        `SELECT * FROM images
-        WHERE id = ?`, [id]
-    )
-    return newPhoto[0]
-};
+  const newPhoto = await pool.query(
+    `SELECT * FROM images
+        WHERE id = ?`,
+    [id]
+  );
+  return newPhoto[0];
+}
 
 // updating the image table
 export async function updatePhoto(image_url, id) {
-    const newPhoto = await pool.query(
-        `UPDATE images
+  const newPhoto = await pool.query(
+    `UPDATE images
         SET imageUrl = ? 
-        WHERE id = ?`, [image_url, id]
-    )
-    return newPhoto[0]
-};
+        WHERE id = ?`,
+    [image_url, id]
+  );
+  return newPhoto[0];
+}
 
 //adding menu items
 export async function addMenu(title, category, price, description) {
-    const newMenu = await pool.query(
-        `INSERT INTO menu (title, category, price, description)
-        VALUES (?, ?, ?, ?)`, [title, category, price, description]
-    )
-    return newMenu[0]
-};
+  const newMenu = await pool.query(
+    `INSERT INTO menu (title, category, price, description)
+        VALUES (?, ?, ?, ?)`,
+    [title, category, price, description]
+  );
+  return newMenu[0];
+}
 
 // deleting multiple rows depending on what the delete form sends
 //fix error when deleting multiple select options.
 export async function removeMenu(id) {
-    const deleteMenu = await pool.query(
-        `DELETE FROM menu
-        WHERE id IN (?)`, [id]
-    )
-    return deleteMenu[0]
-};
+  const deleteMenu = await pool.query(
+    `DELETE FROM menu
+        WHERE id IN (?)`,
+    [id]
+  );
+  return deleteMenu[0];
+}
 
 //creating reservations without being logged in.
 export async function defaultReservation(date, time, name, email) {
-    const defReservation = await pool.query(
-        `INSERT INTO reservations(date, time, name, email)
-        VALUES(?, ?, ?, ?)`, [date, time, name, email]
-    )
-    return defReservation[0]
-};
+  const defReservation = await pool.query(
+    `INSERT INTO reservations(date, time, name, email)
+        VALUES(?, ?, ?, ?)`,
+    [date, time, name, email]
+  );
+  return defReservation[0];
+}
 
 // selecting a specific reservation date then summing up all the covers
 export async function reservationDate(date) {
-    const specificDate = await pool.query(
-        `SELECT SUM(covers) AS total_covers
+  const specificDate = await pool.query(
+    `SELECT SUM(covers) AS total_covers
         FROM  reservations
-        WHERE date = ?`, [date]
-    )
-    return specificDate[0]
+        WHERE date = ?`,
+    [date]
+  );
+  return specificDate[0];
 }
 
 //selecting opening days and hours
 export async function getHours() {
-    const allHours = await pool.query(
-        `SELECT *
+  const allHours = await pool.query(
+    `SELECT *
         FROM hours`
-    )
-    return allHours[0]
+  );
+  return allHours[0];
 }
 
 // selecting specific days and hours
 export async function specificHours(id) {
-    const specHours = await pool.query(
-        `SELECT * 
+  const specHours = await pool.query(
+    `SELECT * 
         FROM hours
-        WHERE id = ?`, [id]
-    )
-    return specHours[0]
+        WHERE id = ?`,
+    [id]
+  );
+  return specHours[0];
 }
 
 //updating the hours table
-export async function updateHours(first_opening, first_closing, second_opening, second_closing, id) {
-    const updatedHours = await pool.query(
-        `UPDATE hours
+export async function updateHours(
+  first_opening,
+  first_closing,
+  second_opening,
+  second_closing,
+  id
+) {
+  const updatedHours = await pool.query(
+    `UPDATE hours
         SET
         first_opening = ?, first_closing = ?, second_opening = ?, second_closing = ?
-        WHERE id = ?`, [first_opening, first_closing, second_opening, second_closing, id]
-    )
-    return updatedHours[0]
+        WHERE id = ?`,
+    [first_opening, first_closing, second_opening, second_closing, id]
+  );
+  return updatedHours[0];
 }
 
 //selecting specific user
 export async function getUser(id) {
-    const specUser = await pool.query(
-        `SELECT * FROM users
-        WHERE id = ?`, [id]
-    )
-    return specUser[0]
+  const specUser = await pool.query(
+    `SELECT * FROM users
+        WHERE id = ?`,
+    [id]
+  );
+  return specUser[0];
 }
